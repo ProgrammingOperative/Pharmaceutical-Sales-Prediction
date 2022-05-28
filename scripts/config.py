041e45b7a61416ed712ca89b7e2e1c6c950bb61a
waCira <wachura11t@gmail.com>
2022-05-28 10:01:51 +0300

    Serialization

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Config:
   RANDOM_SEED = 42
   ASSETS_PATH = Path("../")
-  REPO = "D:\10 Academy\Test\Pharmaceutical-Sales-Prediction"
+  REPO = "D:/10 Academy/Test/Pharmaceutical-Sales-Prediction"
   DATASET_FILE_PATH = "data/AdSmartABdata.csv"
   DATASET_PATH = ASSETS_PATH / "data"
   FEATURES_PATH = ASSETS_PATH / "features"
